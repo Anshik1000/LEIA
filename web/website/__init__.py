2fbf07d40ce699eb53d444db83e14e86a730f722
Anshik1000 <anshik6@gmail.com>
2024-04-27 22:12:35 +0530

    .

--- web/website/__init__.py
+++ web/website/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import pymysql
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY']='AKSJDHWUIAHSDKHJWIASHA'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/leia'
     db = SQLAlchemy(app)
+    
     db.init_app(app)
     from .views import views
     from .auth import auth
